d535d4295f6a96db41a888b1b66dc8a65ae427b1
michael zhou <michaelzhou09@hotmail.com>
2020-06-04 01:58:07 +0000

    COMPX341-20 A4 VERSION 1

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -7,7 +7,7 @@ function AppContainer(props) {
 
     const [responseData, setResponseData] = useState('');
 
-    const handleZipChange = async (cityValue) => {
+    const handleCityChange = async (cityValue) => {
         //console.log(`--------- fetchData called city:${cityValue}`)
         const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?appid=6b7b471967dd0851d0010cdecf28f829&units=metric&q=${cityValue},us`)
         const json = await res.json()
